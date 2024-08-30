7a6d69d245dce8ac7dd0d3d360523d2fab2247ea
Rafael Leite <rafael.lprudencio1@gmail.com>
2024-08-30 00:53:53 -0300

    remoção de ai não usada

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -8,10 +8,6 @@ import { Montserrat } from "next/font/google";
 import {
   LayoutDashboard,
   MessageSquare,
-  ImageIcon,
-  VideoIcon,
-  Music,
-  Code,
   Settings
 } from "lucide-react";
 
@@ -33,30 +29,6 @@ const routes = [
     href: "/conversation",
     color: "text-violet-500"
   },
-  /*{
-    label: "Image Generation",
-    icon: ImageIcon,
-    href: "/image",
-    color: "text-pink-700"
-  },
-  {
-    label: "Video Generation",
-    icon: VideoIcon,
-    href: "/video",
-    color: "text-orange-700"
-  },
-  {
-    label: "Music Generation",
-    icon: Music,
-    href: "/music",
-    color: "text-emerald-500"
-  },
-  {
-    label: "Code Generation",
-    icon: Code,
-    href: "/code",
-    color: "text-green-700"
-  },*/
   {
     label: "Configurações",
     icon: Settings,
